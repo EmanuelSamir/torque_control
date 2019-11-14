58e4481982e34a6fb0debbfbd0e6af1e1d08cafa
EmanuelSamir <emanuel.munoz@utec.edu.pe>
2019-11-13 20:37:16 -0500

    add rbdl function

--- ur_simulation/scripts/simple_bangbang.py
+++ ur_simulation/scripts/simple_bangbang.py
@@ -7,6 +7,8 @@ import rospy
 import actionlib
 from control_msgs.msg import *
 from trajectory_msgs.msg import *
+from copy import copy
+import rbdl
 
 
 
@@ -14,33 +16,46 @@ from trajectory_msgs.msg import *
 class Robot:
 	def  __init__(self):
 		# Measured data initiation
+		self.deltaT = 0.01
+
 		self.q = []		# Joint position
-		self.qd = []	# Joint velocities 
+		self.qd = np.array([])	# Joint velocities
+		self.qdd = np.array([])	# Joint acceleration
 		self.X = []		# Spatial position: x-y-z
-		self.torque = []# Estimated actual torque
 		self.joint_names = []
 
 
 		# Controller data
-		self.q0 = [-np.pi/4., -0.4, 0, 0, 0, 0]
+		self.q0 = np.array([-np.pi/4., 0.0, 0.4, 0, 0, 0])
+		self.torque = np.zeros(self.q0.shape[0]) # Estimated actual torque
 		self.torque_desired = []
 		self.q_desired = []
 
-		# Reading (subscribe)
-		rospy.Subscriber("joint_states", JointState, self.cb_joint_state)
 
 		# Command
-		#self.arm_command = rospy.Publisher("arm_controller/command", JointTrajectory, queue_size = 10)
 		self.client = actionlib.SimpleActionClient('arm_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
 		self.client.wait_for_server()
 
 		self.fjt = FollowJointTrajectoryGoal()
 
+		# Inverse Dynamics
+		self.model = rbdl.loadModel("../urdf/ur5_joint_limited_robot.urdf")
+
+		# Reading (subscribe)
+		rospy.Subscriber("joint_states", JointState, self.cb_joint_state)
 
 	def cb_joint_state(self, data):
 		self.joint_names = data.name
-		self.q = data.position
-		self.qd = data.velocity
+		self.q = np.array(data.position)
+		qd = copy(self.qd)
+		self.qd = np.array(data.velocity)
+
+		if qd.shape == self.qd.shape:
+			self.qdd = (qd - self.qd)/self.deltaT
+
+		self.calculate_torque()
+
+
 
 	def position_control_update(self, q_desired):
 		self.fjt.trajectory = JointTrajectory()
@@ -49,29 +64,30 @@ class Robot:
 												time_from_start = rospy.Duration(0.1))]
 		self.client.send_goal(self.fjt)
 		self.client.wait_for_server()
-		# self.q_desired = q_desired
-		# jt = JointTrajectory()
-		# jt.joint_names = self.joint_names
-		# jt.points = []
-		# jtp = JointTrajectoryPoint()
-		# jtp.positions = self.q_desired
-		# jt.points.append(jtp)
-		# self.arm_command.publish(jt)
+
+	def calculate_torque(self):
+		rbdl.InverseDynamics(self.model, self.q, self.qd, self.qdd, self.torque)
+		print(self.torque)
 
 	def bring_initial_position(self):
-		#for _ in range(1000):
 		self.position_control_update(self.q0)
 
 	def destroy(self):
 		self.client.cancel_goal()
 
+	def simple_bangbang(self, torque_desired):
+		return q_desired
+
+class Controller:
+	def __init__(self):
+		pass
 
 
 def main():
 	rospy.init_node("robot_control", anonymous = True)
 
 	robot = Robot()
-	r = rospy.Rate(10)
+	r = rospy.Rate(100)
 
 	rospy.loginfo("Bring Initial Position")
 	time.sleep(0.5)
@@ -85,4 +101,4 @@ if __name__ == '__main__':
 	try:
 		main()
 	except rospy.ROSInterruptException:
-		robot.destroy()
+		pass#robot.destroy()
